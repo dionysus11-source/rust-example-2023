44cd8a8c670d8c670c5549a924210d319be4cf9a
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2023-01-11 23:30:31 +0900

    update linked list library

--- linked_list_library/src/main.rs
+++ linked_list_library/src/main.rs
@@ -2,4 +2,12 @@ mod slist;
 fn main() {
     let mut list = slist::List::new();
     list.unshift(100);
+    list.unshift(200);
+    list.unshift(300);
+    list.push(400);
+
+    let b = list.get(3).unwrap();
+    //let b = list.get(1).unwrap();
+    //let b = list.get(2).unwrap();
+    println!("result {}",b);
 }
--- linked_list_library/src/slist.rs
+++ linked_list_library/src/slist.rs
@@ -2,30 +2,58 @@ pub struct Node{
     data : isize,
     link : Option<Box<Node>>,
 }
-
 pub struct List{
     head : Option<Box<Node>>,
 }
-
 impl List{
     pub fn new() -> Self{
         Self{head: None}
     }
-
     pub fn unshift(&mut self, val: isize){
        let tmp = Box::new(Node{data : val, link: self.head.take()});
        self.head = Some(tmp);
     }
     pub fn push(&mut self, v:isize){
        let tmp = Box::new(Node{data : v, link: None});
-        match self.head.as_ref(){
+        match self.head{
             None => self.head = Some(tmp),
-            Some(ptr) => {}
+            Some(ref mut ptr) => {
+                let mut pp = ptr;
+                loop{
+                    match pp.link{
+                        None => {
+                            pp.link = Some(tmp);
+                            break;},
+                        Some(ref mut v) =>{
+                            pp = v;
+                        }
+                    }
+                }
+            }
         }
-
-       
     }
-    pub fn get(&self){
-
+    pub fn get(&mut self, index:usize) -> Option<isize>{
+        let mut cnt = 0;
+        let mut ret:Option<isize> = None;
+        match self.head{
+            None => return None,
+            Some(ref mut ptr) => {
+                let mut pp = ptr;
+                loop{
+                            if cnt == index{
+                                return Some(pp.data);
+                            }
+                    match pp.link{
+                        None => return None,
+                        Some(ref mut v) =>{
+                            pp = v;
+                        }
+                    
+                }
+                cnt += 1;
+            }
+        }
+    
+    }
     }
 }
